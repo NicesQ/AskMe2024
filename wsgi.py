5d105b932f780115398ea274fe75d7a1905838a9
Georgiy <kolomytkin2020@gmail.com>
2024-05-29 22:31:41 +0300

    fix

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-def app(environ, start_response):
-        data = b"Hello, World!\n"
-        data += str.encode(environ['QUERY_STRING'])
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-        ])
-        return [data]
